a19bb58ce6fe2860d2e894cc3f57b24c7763719a
mejarisuresh <100571335+mejarisuresh@users.noreply.github.com>
2022-03-30 08:07:49 -0700

    Update renuka.groovy

--- vars/renuka.groovy
+++ vars/renuka.groovy
@@ -1,6 +1,6 @@
 def call(ip,user,credid)
 {
-       sshagent(['credid']) {
+       sshagent(['aaaa']) {
          sh "scp -o StrictHostKeyChecking=no target/*.war ${user}@${ip}:/opt/tomcat8/webapps"
      sh "ssh ${user}@${ip} /opt/tomcat8/bin/startup.sh"
                
